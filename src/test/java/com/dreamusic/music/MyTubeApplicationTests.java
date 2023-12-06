710fb1f0013663fa4ebcdd61f34d018be4b41275
Remy P <remy.petre@gmail.com>
2023-12-06 20:45:23 +0100

    First commit

---
+++ src/test/java/com/dreamusic/music/MyTubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreamusic.music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
